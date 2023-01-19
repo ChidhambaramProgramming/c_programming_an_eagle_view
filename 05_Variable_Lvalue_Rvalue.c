a1b840f720a50fbf776cffe6c2a5ede3e3a7cb68
ChidhambaramProgramming <123047456+ChidhambaramProgramming@users.noreply.github.com>
2023-01-19 11:21:38 +0530

    05_Variable_Lvalue_Rvalue.c

---
+++ 05_Variable_Lvalue_Rvalue.c
@@ -0,0 +1,8 @@
+﻿#include<stdio.h>
+//L value should contain a variable to store and R value should contain a value to be stored.
+int main()
+{
+ int x = 10,y = 2;
+ 10 = y;              // value 10 is not a memory reference its a value.
+ printf("X = %d Y = %d\n",x,y);
+} 
