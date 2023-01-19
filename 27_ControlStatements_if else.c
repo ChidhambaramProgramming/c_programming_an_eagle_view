aec96e017eea83f2329c2e70ca22fc3bdfb347ac
ChidhambaramProgramming <123047456+ChidhambaramProgramming@users.noreply.github.com>
2023-01-19 16:21:48 +0530

    27_Control Statements _ if else statement

---
+++ 27_ControlStatements_if else.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+                      //simple if else
+int main()
+{
+ int x = 10;
+ if(x > 0)
+ {
+  printf("x is positive");
+ }
+ else
+ {
+  printf("x is non-positive");
+ }
+ return 0;
+}
