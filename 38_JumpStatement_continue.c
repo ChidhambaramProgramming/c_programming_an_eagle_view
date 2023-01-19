8ab8c03d601ef165db27f2ac0a61606d06618168
ChidhambaramProgramming <123047456+ChidhambaramProgramming@users.noreply.github.com>
2023-01-19 19:39:41 +0530

    38_Jump Statement : Continue

---
+++ 38_JumpStatement_continue.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+                      //jump statement : continue
+int main()
+{
+ int i;
+
+ for(i=1;i<=10;i++)
+ {
+  if(i%2 == 0)
+  {
+   continue;
+  }
+  printf("%d ",i);
+ }
+ printf("\n");
+ return 0;
+}
