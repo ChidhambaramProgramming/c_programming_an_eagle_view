423e1efd1df4cb34c4f882f70e0a51fe34c99e2d
ChidhambaramProgramming <123047456+ChidhambaramProgramming@users.noreply.github.com>
2023-01-20 09:59:05 +0530

    46_Pointer Dynamic Memory Allocation - 1

---
+++ 46_Pointer_Dynamic memory_1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+ char *name = NULL;
+ name = (char*)malloc(80);
+ if(name != NULL)
+ {
+  printf("Memory allocated successfully !!!\n");
+  strcpy(name,"Chidambaram Bhagavathy");
+  printf("Name : %s\n",name);
+  free(name);
+ }
+ else
+ {
+  printf("Unable to allocate memory !!!");
+ }
+ return 0;
+}
