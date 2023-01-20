90408e5a3d80b6b1ee7f83683a72dca4fbc61930
ChidhambaramProgramming <123047456+ChidhambaramProgramming@users.noreply.github.com>
2023-01-21 00:04:56 +0530

    72_Hidden Facts Switch error

---
+++ 72_Hiddenfacts_Switch_err.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+
+int main()
+
+{
+    
+ enum { SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY }day;
+    
+ day = WEDNESDAY;
+ switch(day) 
+ {
+        
+  case SUNDAY: 
+   printf("Sunday\n"); 
+   break;
+        
+  case MONDAY: 
+   printf("MONDAY\n"); 
+   continue;
+        // Switch has break so dont think it can allow continue as well.
+  case TUESDAY:                   
+   printf("Tuesday\n"); 
+   break;
+    
+ }
+        
+ return 0;
+
+}
+
+
