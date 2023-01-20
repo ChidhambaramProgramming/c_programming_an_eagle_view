9e365f374d78661e2b213da3abf3157c7d1d8f0a
ChidhambaramProgramming <123047456+ChidhambaramProgramming@users.noreply.github.com>
2023-01-20 15:40:06 +0530

    69_Volatile

---
+++ 69_Volatile.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+
+#pragma GCC optimize ("O3")
+
+
+
+int main(void) 
+{
+	
+ const volatile int local = 10;
+	 //use of volatile keyword forces the compiler to always refer the actual memory location.
+ int *ptr = (int*) &local;
+
+	
+ printf("Initial value of local : %d \n", local);
+
+	
+ *ptr = 100;
+
+	
+ printf("Modified value of local : %d \n", local);
+
+	
+ return 0;
+
+}
