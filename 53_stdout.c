4692ca4c1269e7eeb341a5c7d591b3d7802e78b9
ChidhambaramProgramming <123047456+ChidhambaramProgramming@users.noreply.github.com>
2023-01-20 11:39:26 +0530

    53_Standard Output

---
+++ 53_stdout.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+                      //stdout
+int main()
+{
+ printf("Happy new year");
+ printf("\nprintf : prints string to console");
+ fprintf(stdout,"\nWelcome to 2023");
+ printf("\nfprintf : puts string into stdout stream);
+
+ return 0;
+}
