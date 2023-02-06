cb26b97bc0754e7bde9ffb750ceb848b36b76e2d
ChidhambaramProgramming <123047456+ChidhambaramProgramming@users.noreply.github.com>
2023-02-06 06:37:15 +0530

    Update 80_HiddenFacts_ArrayPointer.c

--- 80_HiddenFacts_ArrayPointer.c
+++ 80_HiddenFacts_ArrayPointer.c
@@ -1,54 +1,44 @@
 #include<stdio.h>
+int main() {
+    int i, a[10] = {1,2,3,4,5,6,7,8,9,10};
+    int (*ptr_a)[10] = &a; //pointer to array of 10 integer
+    int *ptr_aa = a; //pointer to integer
 
-
-
-int main() 
-{
-    
- int i, a[10] = {1,2,3,4,5,6,7,8,9,10};
-    
- int (*ptr_a)[10] = &a; //pointer to array of 10 integer 
-    
- int *ptr_aa = a; //pointer to integer
- 
-    
-    
- printf("Displaying data from Array (a) : \n");
-    
- for(i=0; i<10; i++) 
- {
-        
-  printf("-> %d ", a[i]);
-    
- }
-    
- printf("\n");
-    
- printf("Displaying data from pointer to a (ptr_a) : \n");
-    
- for(i=0; i<10; i++) 
- {
-        
-  printf("-> %d ", (*ptr_a)[i]);
-    
- }
-    
- printf("\n");
-    
- printf("Displaying data from pointer to a (ptr_aa) : \n");
-    
- for(i=0; i<10; i++) 
- {
-        
-  printf("-> %d ", ptr_aa[i]);
-    
- }
-    
- printf("\n");
-
-    
-    
- return 0;
-
+    printf("Displaying data from Array (a) : \n");
+    for(i=0; i<10; i++) {
+        printf("-> %d ", a[i]);
+    }
+    printf("\nDisplaying data from pointer to a (ptr_a) : \n");
+    for(i=0; i<10; i++) {
+        printf("-> %d ", (*ptr_a)[i]);
+    }
+    printf("\nDisplaying data from pointer to a (ptr_a) using array convention: \n");
+    for(i=0; i<10; i++) {
+        printf("-> %d ", (*ptr_a)[i]);
+    }
+    printf("\nDisplaying data from pointer to a (ptr_a) using pointer convention: \n");
+    for(i=0; i<10; i++) {
+        printf("-> %d ", *((*ptr_a)+i));
+    }
+    printf("\nMemory walk through (ptr_a): \n");
+    for(i=0; i<10; i++, ptr_a++) {
+        printf("-> %d ", ptr_a);
+    }
+    printf("\nDisplaying data from pointer to a (ptr_aa) using arrray convention: \n");
+    for(i=0; i<10; i++) {
+        printf("-> %d ", ptr_aa[i]);
+    }
+    printf("\nDisplaying data from pointer to a (ptr_aa) using pointer convention: \n");
+    for(i=0; i<10; i++, ptr_aa++) {
+        printf("-> %d ", *ptr_aa);
+    }
+    printf("\nMemory walk through (ptr_aa): \n");
+    for(i=0; i<10; i++, ptr_aa++) {
+        printf("-> %d ", ptr_aa);
+    }
+    printf("\n");
+    return 0;
 }
+
+
 
